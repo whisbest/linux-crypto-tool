21d3929d02a93363274363f73c1a5009b154426f
whisbest <2022868472@qq.com>
2024-12-24 11:15:16 +0800

    Improve error handling in crypto functions

--- crypto.c
+++ crypto.c
@@ -1,46 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "crypto.h"
+#include <errno.h> // 用于获取错误代码
+#include "crypto.h" // 确保BUFFER_SIZE在此文件中定义
+
+// 改进的错误处理函数
+void print_error(const char* message, int errnum) {
+    fprintf(stderr, "错误: %s (错误代码: %d)\n", message, errnum);
+}
 
 int encrypt_file(const char* input_file, const char* output_file, int key) {
     FILE *in, *out;
     unsigned char buffer[BUFFER_SIZE];
     size_t bytes_read;
+    int err;
 
     // 打开输入文件
     in = fopen(input_file, "rb");
     if (!in) {
-        print_error("无法打开输入文件");
+        print_error("无法打开输入文件", errno);
         return -1;
     }
 
     // 打开输出文件
     out = fopen(output_file, "wb");
     if (!out) {
-        print_error("无法创建输出文件");
+        print_error("无法创建输出文件", errno);
         fclose(in);
         return -1;
     }
 
     // 简单的异或加密
     while ((bytes_read = fread(buffer, 1, BUFFER_SIZE, in)) > 0) {
+        if (ferror(in)) {
+            print_error("读取输入文件时发生错误", errno);
+            fclose(in);
+            fclose(out);
+            return -1;
+        }
         for (size_t i = 0; i < bytes_read; i++) {
             buffer[i] ^= key;
         }
-        fwrite(buffer, 1, bytes_read, out);
+        if (fwrite(buffer, 1, bytes_read, out) != bytes_read) {
+            print_error("写入输出文件时发生错误", errno);
+            fclose(in);
+            fclose(out);
+            return -1;
+        }
+    }
+
+    if (feof(in)) {
+        // 文件正常结束，无需处理
+    } else {
+        // 非正常结束，打印错误信息
+        print_error("文件读取异常", errno);
     }
 
     fclose(in);
     fclose(out);
     return 0;
-}
-
-int decrypt_file(const char* input_file, const char* output_file, int key) {
-    // 解密与加密使用相同的异或算法
-    return encrypt_file(input_file, output_file, key);
-}
-
-void print_error(const char* message) {
-    fprintf(stderr, "错误: %s\n", message);
 }
